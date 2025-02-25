3039774787c09c9cee3c343c3a2a68070ebff5b7
l-Oskar <fediuk_v@protonmail.com>
2025-02-25 15:46:43 +0200

    vite configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/StarWars/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
